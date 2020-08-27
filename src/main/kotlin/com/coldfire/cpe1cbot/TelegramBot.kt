c99caf975bbb6dd427752767491e18011a370661
Oliver Rhyme Añasco <35908542+OliverRhyme@users.noreply.github.com>
2020-08-27 16:49:56 +0800

    Fix heroku port problem
    Signed-off-by: Oliver Rhyme Añasco <35908542+OliverRhyme@users.noreply.github.com>

--- src/main/kotlin/com/coldfire/cpe1cbot/TelegramBot.kt
+++ src/main/kotlin/com/coldfire/cpe1cbot/TelegramBot.kt
@@ -1,29 +1,23 @@
 package com.coldfire.cpe1cbot
 
 import com.elbekD.bot.Bot
+import com.elbekD.bot.server
 import java.util.regex.Pattern
 
 
 fun main(args: Array<String>) {
     val token = "1360445525:AAHqvf4oHVFf2sw1GBvqJCg22BOBS95JGF0"
     val username = "@OliverRhyme_bot"
-    val bot = Bot.createPolling(username, token)
+    val bot = Bot.createWebhook(username, token)
     bot.onCommand("/start") { msg, _ ->
         bot.sendMessage(msg.chat.id, "Hello mga maderpaker!")
     }
     bot.onCommand("/weak") { msg, _ ->
         bot.sendMessage(msg.chat.id, "Si Leomar")
     }
-//    bot.onCommand("/Leomar? unsay ass?ignment") {msg, _ ->
-//        bot.sendMessage(msg.chat.id, "SIBAT PA YAWAA KA!")
-//    }
     bot.onMessage {
-//        val pattern: Pattern = Pattern.compile("\\b(yawa)\\b")
         val matcher = it.text?.toLowerCase()
         when (matcher) {
-//            "yawa", "pota" -> {
-//                bot.sendMessage(it.chat.id, "Don't say bad words God is watching you")
-//            }
             "goodmorning", "ohaiyo" -> {
                 bot.sendMessage(it.chat.id, "Goodmorning!! Start your day with a cup of coffee!")
             }

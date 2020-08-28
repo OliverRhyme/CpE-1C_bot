79c8ef22cc91bbb54442c5e325900ff2ea55027f
Oliver Rhyme Añasco <35908542+OliverRhyme@users.noreply.github.com>
2020-08-28 23:42:33 +0800

    Add leet map and decouple swear detection
    Signed-off-by: Oliver Rhyme Añasco <35908542+OliverRhyme@users.noreply.github.com>

--- src/main/kotlin/com/coldfire/cpe1cbot/SwearDetector.kt
+++ src/main/kotlin/com/coldfire/cpe1cbot/SwearDetector.kt
@@ -2,9 +2,66 @@ package com.coldfire.cpe1cbot
 
 class SwearDetector {
 
+    private val swearRegex: Regex
+
+    init {
+        swearRegex = getProcessedRegex()
+    }
+
+//    private fun getProcessedSwearList(): List<String> {
+//
+//        val tempList = mutableListOf<String>()
+//
+//        for (swear in Constants.swearList) {
+//
+//            val tempWord = StringBuilder()
+//
+//            for (s in swear) {
+//
+//                val converted = Constants.leetMap.getValue(s)
+//
+//                if (converted.length > 1) tempWord.append("[$converted]")
+//                else tempWord.append(s)
+//
+//            }
+//
+//            tempList.add(tempWord.toString())
+//        }
+//
+//        return tempList
+//    }
+
+
+    private fun getProcessedRegex(): Regex {
+//        val swearList = getProcessedSwearList()
+        val swearList = Constants.swearList
+
+        val regexPattern = swearList.joinToString(
+            prefix = "(",
+            separator = "|",
+            postfix = ")",
+            transform = {
+                val tempWord = StringBuilder()
+
+                for (s in it) {
+
+                    val converted = Constants.leetMap.getValue(s)
+
+                    if (converted.length > 1) tempWord.append("[$converted]")
+                    else tempWord.append(s)
+
+                }
+                return@joinToString tempWord
+            }
+        )
+        return regexPattern.toRegex()
+
+
+    }
+
     // TODO decouple and incorporate leet
     fun hasSwear(text: String?): Boolean {
         return if (text.isNullOrBlank()) false
-        else Regex("(y([a@])w[a@]|p[0o][t+][a@])").containsMatchIn(text)
+        else swearRegex.containsMatchIn(text)
     }
 }

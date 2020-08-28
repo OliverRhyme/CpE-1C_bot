53244632c1626e916b4e97981ec36abd5282d385
Oliver Rhyme Añasco <35908542+OliverRhyme@users.noreply.github.com>
2020-08-28 12:38:35 +0800

    Revert to polling
    Signed-off-by: Oliver Rhyme Añasco <35908542+OliverRhyme@users.noreply.github.com>

--- src/main/kotlin/com/coldfire/cpe1cbot/TelegramBot.kt
+++ src/main/kotlin/com/coldfire/cpe1cbot/TelegramBot.kt
@@ -2,29 +2,40 @@ package com.coldfire.cpe1cbot
 
 import com.elbekD.bot.Bot
 import com.elbekD.bot.server
+import com.elbekD.bot.types.WebhookInfo
 
 
 fun main() {
     val token = "1360445525:AAHqvf4oHVFf2sw1GBvqJCg22BOBS95JGF0"
     val username = "@OliverRhyme_bot"
-    val bot = Bot.createWebhook(username, token) {
-        this.url = "https://cpe1c-bot.herokuapp.com/"
-        server {
-            val portNumber = System.getenv("PORT")
-            portNumber?.let {
-                port = Integer.parseInt(portNumber)
-            }
-            this.host = "localhost"
-        }
+    val bot = Bot.createPolling(username, token)
+//        this.url = "https://cpe1c-bot.herokuapp.com/"
+//
+//        server {
+//            val portNumber = System.getenv("PORT")
+//            portNumber?.let {
+//                port = Integer.parseInt(portNumber)
+//            }
+//            this.host = "localhost"
+//        }
+//    }
+
+//    bot.getWebhookInfo()
+
+    bot.onCommand("/test") { msg, _ ->
+        bot.sendMessage(msg.chat.id, "${System.getenv("PORT")} test")
     }
 
-
     bot.onCommand("/start") { msg, _ ->
-        bot.sendMessage(msg.chat.id, "Hello mga maderpaker!")
+        bot.sendMessage(msg.chat.id, "test")
     }
     bot.onCommand("/weak") { msg, _ ->
         bot.sendMessage(msg.chat.id, "Si Leomar")
     }
+
+
+
+
     bot.onMessage {
         when (val matcher = it.text?.toLowerCase()) {
             "goodmorning", "ohaiyo" -> {
@@ -32,7 +43,7 @@ fun main() {
             }
             else -> {
                 matcher?.let { swear ->
-                    if (Regex("\\b(y([a@])wa|p[0o][t+][a@])\\b").containsMatchIn(swear)) {
+                    if (Regex("\\b(y([a@])w[a@]|p[0o][t+][a@])\\b").containsMatchIn(swear)) {
                         bot.sendMessage(it.chat.id, "Don't say bad words God is watching you")
                     }
                 }
@@ -45,6 +56,7 @@ fun main() {
         bot.sendMessage(msg.chat.id, "Si Oliver and Leomar")
     }
     bot.start()
+
 }
 
 

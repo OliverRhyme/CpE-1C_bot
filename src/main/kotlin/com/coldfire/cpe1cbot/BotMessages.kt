fcb0e5563ad9b4e922eb5b670a99b2a965280c18
Oliver Rhyme Añasco <35908542+OliverRhyme@users.noreply.github.com>
2020-09-01 22:04:12 +0800

    Convert messages to lazy property
    Signed-off-by: Oliver Rhyme Añasco <35908542+OliverRhyme@users.noreply.github.com>

--- src/main/kotlin/com/coldfire/cpe1cbot/BotMessages.kt
+++ src/main/kotlin/com/coldfire/cpe1cbot/BotMessages.kt
@@ -3,55 +3,68 @@ package com.coldfire.cpe1cbot
 import java.util.*
 
 object BotMessages {
-    val creditsMessage = """
-                        This bot is written by:
-                        
-                        Oliver Rhyme G. Añasco
-                        
-                        📧 oliverrhyme.anasco@g.msuiit.edu.ph
-                        <a href = "https://fb.me/oliver.rhyme">Facebook</a>
-                        <a href="tg://user?id=640358656">Telegram</a>
-                        <a href="https://github.com/OliverRhyme">Github</a>
-                        
-                        Leomar T. Villaruel
-                        
-                        📧 leomart.villaruel@gmail.com
-                        <a href = "https://fb.me/leomar.villareal.1">Facebook</a>
-                        <a href="tg://user?id=1302941940">Telegram</a>
-                        <a href="https://github.com/LeomarVillaruel">Github</a>
+    val creditsMessage by lazy {
+        """
+        This bot is written by:
+        
+        Oliver Rhyme G. Añasco
+        
+        📧 oliverrhyme.anasco@g.msuiit.edu.ph
+        <a href = "https://fb.me/oliver.rhyme">Facebook</a>
+        <a href="tg://user?id=640358656">Telegram</a>
+        <a href="https://github.com/OliverRhyme">Github</a>
+        
+        Leomar T. Villaruel
+        
+        📧 leomart.villaruel@gmail.com
+        <a href = "https://fb.me/leomar.villareal.1">Facebook</a>
+        <a href="tg://user?id=1302941940">Telegram</a>
+        <a href="https://github.com/LeomarVillaruel">Github</a>
 
-                        
-                        100% written in Kotlin with 💖
-                        
-                        Library used:
-                        <a href = "https://github.com/kotlin-telegram-bot/kotlin-telegram-bot">Kotlin Telegram Bot</a>
-                        
-                        CpE-1C Telegram Bot V${getVersion()}
+        
+        100% written in Kotlin with 💖
+        
+        Library used:
+        <a href = "https://github.com/kotlin-telegram-bot/kotlin-telegram-bot">Kotlin Telegram Bot</a>
+        
+        CpE-1C Telegram Bot V${getVersion()}
 
-                    """.trimIndent()
+        """.trimIndent()
+    }
 
-    val startMessage = """
-                        Welcome this bot will answer you FAQ
-                        please refer to commands you can try below
+    val startMessage by lazy {
+        """
+        Welcome this bot will answer you FAQ
+        please refer to commands you can try below
                         
-                            """.trimIndent()
-
-    val announcementMessage = """
-                                        There are currently no announcement please stay tuned for updates.
                         
-                                    """.trimIndent()
+        """.trimIndent()
+    }
 
-    val newMemberMessage = """
-                                Welcome to CpE-1C group chat <a href="tg://user?id=%d">%s</a>.
-                                Please adhere to this group chat rules and regulation.
+    val announcementMessage by lazy {
+        """
+        There are currently no announcement please stay tuned for updates.
+                        
+        """.trimIndent()
+    }
+
+    val newMemberMessage by lazy {
+        """
+        Welcome to CpE-1C group chat <a href="tg://user?id=%d">%s</a>.
+        Please adhere to this group chat rules and regulation.
                                 
-                                """.trimIndent()
-    val adviceMessage = """
-                                Drink 8 glass of water.
-                                Stay hydrated and don't forget to always smile :)
-    """.trimIndent()
-    const val swearMessage = "Don't say bad words God is watching you"
-    const val morningMessage = "Goodmorning %s!! Start your day with a cup of coffee!"
+        """.trimIndent()
+    }
+    val adviceMessage by lazy {
+        """
+        Drink 8 glass of water.
+        Stay hydrated and don't forget to always smile :)
+        """.trimIndent()
+    }
+
+    val swearMessage by lazy { "Don't say bad words God is watching you" }
+
+    val morningMessage by lazy { "Goodmorning %s!! Start your day with a cup of coffee!" }
 
     private fun getVersion(): String {
         val properties = Properties()
